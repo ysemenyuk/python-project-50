3332a692fd5b07519ab57bce170ca11cc6ca85b4
ysemenuk <invent@list.ru>
2022-11-14 23:34:40 +0300

    step 2

---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import argparse
+
+
+parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
+parser.add_argument('first_file', type=str)
+parser.add_argument('second_file', type=str)
+parser.add_argument('-f', '--format', type=str, help='set format of output')
+args = parser.parse_args()
+# print(args.accumulate(args.integers))
